4161a1b39d721f8fd6b13ee3637ad23a513ed221
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2018-11-26 16:03:36 -0500

    Updates

--- precache-manifest.2883635f8acd8b1a884a6572c16ef54c.js
+++ precache-manifest.2883635f8acd8b1a884a6572c16ef54c.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/MyReactPortfolio/static/js/runtime~main.d32ff011.js"
   },
   {
-    "revision": "0deb9752af469cc4ea19",
-    "url": "/MyReactPortfolio/static/js/main.0deb9752.chunk.js"
+    "revision": "868a1020fdc2fad8fcdc",
+    "url": "/MyReactPortfolio/static/js/main.868a1020.chunk.js"
   },
   {
     "revision": "347b32f7ec9129089eea",
     "url": "/MyReactPortfolio/static/js/1.347b32f7.chunk.js"
   },
   {
-    "revision": "0deb9752af469cc4ea19",
-    "url": "/MyReactPortfolio/static/css/main.dd9aaf51.chunk.css"
+    "revision": "868a1020fdc2fad8fcdc",
+    "url": "/MyReactPortfolio/static/css/main.541d0679.chunk.css"
   },
   {
     "revision": "347b32f7ec9129089eea",
     "url": "/MyReactPortfolio/static/css/1.9df77821.chunk.css"
   },
   {
-    "revision": "e8384de521c4b83d125538005a81abe9",
+    "revision": "a61ffb2867a938219074566099368c3a",
     "url": "/MyReactPortfolio/index.html"
   }
 ];
