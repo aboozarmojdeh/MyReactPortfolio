cc36406c593caa0ab6aed6178ecc9bd91036c776
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2018-12-10 19:08:01 -0500

    Initial commit to add build folder for gh-pages

--- build/precache-manifest.cbeac7a905536ee75c64be02b15f2827.js
+++ build/precache-manifest.cbeac7a905536ee75c64be02b15f2827.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/MyReactPortfolio/static/js/runtime~main.d32ff011.js"
   },
   {
-    "revision": "47c2870edad1130348a8",
-    "url": "/MyReactPortfolio/static/js/main.47c2870e.chunk.js"
+    "revision": "e80176841cf1ee69a3b1",
+    "url": "/MyReactPortfolio/static/js/main.e8017684.chunk.js"
   },
   {
     "revision": "347b32f7ec9129089eea",
     "url": "/MyReactPortfolio/static/js/1.347b32f7.chunk.js"
   },
   {
-    "revision": "47c2870edad1130348a8",
+    "revision": "e80176841cf1ee69a3b1",
     "url": "/MyReactPortfolio/static/css/main.541d0679.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/MyReactPortfolio/static/css/1.9df77821.chunk.css"
   },
   {
-    "revision": "fa4e34282cea0c11a31c7d4a682fe967",
+    "revision": "184a9536a385dc2414fe39afc6043219",
     "url": "/MyReactPortfolio/index.html"
   }
 ];
